5cb24ce6b0de79fe26dfd4add6c6d35c6bfe94bd
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2022-01-01 13:56:14 -0300

    feat: implement argon2 and refact method

--- src/infraestructure/encrypter/argon2-encrypter.ts
+++ src/infraestructure/encrypter/argon2-encrypter.ts
@@ -1,7 +1,8 @@
 import { HashUserPassword } from "@/domain/authentication/hash-user-password";
-
+import * as argon2 from "argon2";
 export class Argon2Encrypter implements HashUserPassword {
-  async hashPassword(password: string): Promise<any> {
-    return "";
+  async hashPassword(password: string): Promise<string> {
+    const hashedPassword = await argon2.hash(password);
+    return hashedPassword;
   }
 }
--- tests/infraestructure/encrypter/argon2-ecrypter.spec.ts
+++ tests/infraestructure/encrypter/argon2-ecrypter.spec.ts
@@ -4,13 +4,26 @@ import { Argon2Encrypter } from "@/infraestructure/encrypter/argon2-encrypter";
 describe("argon2 encrypter test", () => {
   let argonEncrypter: MockProxy<Argon2Encrypter>;
 
+  const password = "any_password";
+  const hash = "any_hashed_password";
+
   beforeAll(() => {
     argonEncrypter = mock();
+  });
+
+  beforeEach(() => {
+    argonEncrypter.hashPassword.mockResolvedValueOnce(hash);
   });
 
   test("should call argon2 encrypter with password", () => {
     const argonEncrypterSpy = jest.spyOn(argonEncrypter, "hashPassword");
     argonEncrypter.hashPassword("any_password");
     expect(argonEncrypterSpy).toHaveBeenCalled();
+  });
+
+  test("should return a hashed password", async () => {
+    const hashedPassword = await argonEncrypter.hashPassword(password);
+
+    expect(hashedPassword).toEqual(hash);
   });
 });
