6725df359907b7ea8e6728c3cdebe9057b04227b
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2022-01-03 21:03:48 -0300

    feat: create mocked class RatingEntity

--- tests/domain/rating/rating.spec.ts
+++ tests/domain/rating/rating.spec.ts
@@ -1,3 +1,9 @@
+import { Rating } from "./rating";
+
 describe("Rating Entity", () => {
   test("should call create rating", () => {});
 });
+
+export class RatingEntity {
+  createRating(): void {}
+}
