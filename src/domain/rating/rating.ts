4f81b2039dd54d9ed6e5a0b2be652e8431318279
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2022-01-03 21:21:37 -0300

    chore: move RatingEntity for production file

--- src/domain/rating/rating.ts
+++ src/domain/rating/rating.ts
@@ -5,3 +5,24 @@ export interface Rating {
   comment?: string;
   isActive: boolean;
 }
+
+export interface createRatingDto {
+  userId: string;
+  movieId: string;
+  score: number;
+  comment?: string;
+}
+
+export class RatingEntity {
+  createRating(rating: createRatingDto): void {}
+
+  validateComment(comment: string): boolean {
+    let isCommentValid = false;
+
+    if (comment.length < 10) {
+      isCommentValid = false;
+    }
+
+    return isCommentValid;
+  }
+}
