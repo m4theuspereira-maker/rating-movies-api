f7d0968579701cc7d49f603cef95006d0f3760b8
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2022-01-06 10:56:58 -0300

    ensure: method call with right params

---
+++ tests/domain/date/date-helper.spec.ts
@@ -0,0 +1,40 @@
+import { mock, MockProxy } from "jest-mock-extended";
+
+describe("hash-user-password", () => {
+  let dateHelper: MockProxy<DateHelper>;
+  const AGE = 22;
+  const BIRTH_DATE = "01/01/2000";
+
+  beforeAll(() => {
+    dateHelper = mock();
+    dateHelper.getAgeByBirthDate.mockImplementationOnce(() => AGE);
+  });
+
+  test("should call method from data helper", () => {
+    const getAgeByBirthDateSpy = jest.spyOn(dateHelper, "getAgeByBirthDate");
+
+    dateHelper.getAgeByBirthDate(BIRTH_DATE);
+
+    expect(getAgeByBirthDateSpy).toHaveBeenCalled();
+  });
+
+  test("should call method from data helper", () => {
+    const getAgeByBirthDateSpy = jest.spyOn(dateHelper, "getAgeByBirthDate");
+
+    dateHelper.getAgeByBirthDate(BIRTH_DATE);
+
+    expect(getAgeByBirthDateSpy).toHaveBeenCalledWith(BIRTH_DATE);
+  });
+
+  test("should call method from data helper", () => {
+    const getAgeByBirthDateSpy = jest.spyOn(dateHelper, "getAgeByBirthDate");
+
+    dateHelper.getAgeByBirthDate(BIRTH_DATE);
+
+    expect(getAgeByBirthDateSpy).toHaveBeenCalledWith(BIRTH_DATE);
+  });
+});
+
+export interface DateHelper {
+  getAgeByBirthDate(birthDate: string): number;
+}
