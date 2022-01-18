52ed0ecaad01f424daccf8b14298b383db3f4923
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2022-01-18 17:32:23 -0300

    chore: remove usless spaces

--- src/domain/authentication/token.ts
+++ src/domain/authentication/token.ts
@@ -3,11 +3,9 @@ export interface GenerateTokenDto {
   type: string;
   isActive: boolean;
 }
-
 export interface TokenEncoder {
   generateToken(generateTokenDto: GenerateTokenDto): Promise<string>;
 }
-
 export interface TokenDecoder {
   decodeToken(token: string): Promise<any>;
 }
