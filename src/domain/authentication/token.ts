cabfcfd7bff079b3e72fc69de4926ddbaa1d2bce
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2022-01-28 09:16:27 -0300

    refactor: refact token interface return type

--- src/domain/authentication/token.ts
+++ src/domain/authentication/token.ts
@@ -1,10 +1,17 @@
+import { ServerError } from "@/infraestructure/errors/server-error";
+
 export interface GenerateTokenDto {
   id: string;
   type: string;
+  password: string;
   isActive: boolean;
 }
+
+export type TokenCreationResult = ServerError | string;
 export interface TokenEncoder {
-  generateToken(generateTokenDto: GenerateTokenDto): Promise<string>;
+  generateToken(
+    generateTokenDto: GenerateTokenDto
+  ): Promise<TokenCreationResult>;
 }
 export interface TokenDecoder {
   decodeToken(token: string): Promise<any>;
