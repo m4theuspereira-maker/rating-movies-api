e42bcf4e56ff6cb31e4dcab50607048313008c6a
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2022-01-29 12:56:11 -0300

    fix: refactor tests and remove useless mockproxy

--- tests/domain/rating/rating.spec.ts
+++ tests/domain/rating/rating.spec.ts
@@ -1,10 +1,8 @@
-import { MockProxy, mock } from "jest-mock-extended";
 import { createRatingDto, RatingEntity } from "@/domain/rating/rating";
 import { BusinessError } from "@/domain/errors/business-error";
 
 describe("Rating Entity", () => {
   let ratingEntitySut: RatingEntity;
-  let ratingEntity: MockProxy<RatingEntity>;
   const RATING_DATA: createRatingDto = {
     userId: "any_user_id",
     movieId: "any_movie_id",
@@ -14,24 +12,23 @@ describe("Rating Entity", () => {
 
   beforeAll(() => {
     ratingEntitySut = new RatingEntity();
-    ratingEntity = mock();
   });
   test("should call create rating", () => {
-    const ratingEntitySpy = jest.spyOn(ratingEntity, "createRating");
-    ratingEntity.createRating(RATING_DATA);
+    const ratingEntitySpy = jest.spyOn(ratingEntitySut, "createRating");
+    ratingEntitySut.createRating(RATING_DATA);
     expect(ratingEntitySpy).toHaveBeenCalled();
   });
 
   test("should call createRating with rating data", () => {
-    ratingEntity.createRating(RATING_DATA);
-    expect(ratingEntity.createRating).toBeCalledWith(RATING_DATA);
+    ratingEntitySut.createRating(RATING_DATA);
+    expect(ratingEntitySut.createRating).toBeCalledWith(RATING_DATA);
   });
 
   test("should call validate comment with comment", () => {
-    ratingEntity.validateComment(RATING_DATA.comment!!);
-    expect(ratingEntity.validateComment).toHaveBeenCalledWith(
-      RATING_DATA.comment
-    );
+    const validCommentSpy = jest.spyOn(ratingEntitySut, "validateComment");
+    ratingEntitySut.validateComment(RATING_DATA.comment!!);
+
+    expect(validCommentSpy).toHaveBeenCalledWith(RATING_DATA.comment);
   });
 
   test("should return false if comment has less than 10 characters", () => {
@@ -49,27 +46,24 @@ describe("Rating Entity", () => {
   });
 
   test("should return a business error if an invalid comment", () => {
-    try {
-      const createRatingResult = ratingEntitySut.createRating({
+    expect(() => {
+      ratingEntitySut.createRating({
         comment: "any_com",
         userId: "any_user_id",
         movieId: "any_movie_id",
         score: 3,
       });
-      expect(createRatingResult).toThrow(new BusinessError());
-    } catch (error) {}
+    }).toThrowError(new BusinessError());
   });
 
   test("should return business error of invalid score", () => {
-    try {
-      const validRatinWithComment = ratingEntitySut.createRating({
+    expect(() => {
+      ratingEntitySut.createRating({
         userId: "any_user_id",
         movieId: "any_movie_id",
         score: 6,
       });
-
-      expect(validRatinWithComment).toThrow(new BusinessError());
-    } catch (error) {}
+    }).toThrowError(new BusinessError());
   });
 
   test("should return a valid rating if the data were right", () => {
