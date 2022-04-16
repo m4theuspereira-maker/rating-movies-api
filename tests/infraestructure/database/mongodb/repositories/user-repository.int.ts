d23d8a5fa81129d14da33523388fac1f78f2ec58
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2022-04-16 12:16:39 -0300

    refactor: remove useless imports and classes

--- tests/infraestructure/database/mongodb/repositories/user-repository.int.ts
+++ tests/infraestructure/database/mongodb/repositories/user-repository.int.ts
@@ -1,19 +1,11 @@
-import {
-  IFindAllUsersRepository,
-  ISaveUserRepository
-} from "@/domain/user/repository/user-repository-interface";
-import { findUsersDto, User, USER_ROLES } from "@/domain/user/user";
-import { ServerError } from "@/infraestructure/errors/server-error";
+import { USER_ROLES } from "@/domain/user/user";
 import {
   dropDatabase,
   seedDatabase,
   startDatabase
 } from "../../../../config/test-config/setupDatabaseTests";
-import mongoose from "mongoose";
-import {
-  IModelUser,
-  UserModel
-} from "@/infraestructure/database/mongodb/models/user-entity-model";
+import { UserRepository } from "@/infraestructure/database/mongodb/repositories/user/user-repository";
+import { UserModel } from "@/infraestructure/database/mongodb/models/user-entity-model";
 
 export const VALID_ADM = {
   name: "any name",
@@ -23,30 +15,6 @@ export const VALID_ADM = {
   isActive: true,
   createdAt: new Date()
 };
-
-export class UserRepository
-  implements ISaveUserRepository, IFindAllUsersRepository
-{
-  constructor(private readonly userModel: mongoose.Model<IModelUser>) {}
-  async findUsers(): Promise<findUsersDto[]> {
-    try {
-      const users = (await this.userModel.find()) as unknown as findUsersDto[];
-      return users;
-    } catch (error) {
-      console.error(error);
-      throw new ServerError();
-    }
-  }
-  async saveUser(user: User): Promise<void> {
-    try {
-      const userSaved = await this.userModel.create(user);
-      await userSaved.save();
-    } catch (error) {
-      console.error(error);
-      throw new ServerError();
-    }
-  }
-}
 
 describe("User repository", () => {
   const userRepository = new UserRepository(UserModel);
