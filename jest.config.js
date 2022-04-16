b530c1368c63b313861e8fe935a1b01b250f9efb
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2022-04-16 12:17:01 -0300

    chore: add int tests names in jest

--- jest.config.js
+++ jest.config.js
@@ -5,19 +5,19 @@ module.exports = {
   collectCoverageFrom: [
     "<rootDir>/src/**/*.ts",
     "!<rootDir>/src/main/**",
-    "!<rootDir>/src/**/index.ts",
+    "!<rootDir>/src/**/index.ts"
   ],
-  preset: '@shelf/jest-mongodb',
+  preset: "@shelf/jest-mongodb",
   coverageDirectory: "coverage",
   coverageProvider: "babel",
   moduleNameMapper: {
     "@/tests/(.+)": "<rootDir>/tests/$1",
-    "@/(.+)": "<rootDir>/src/$1",
+    "@/(.+)": "<rootDir>/src/$1"
   },
-  testMatch: ["**/*.spec.ts"],
+  testMatch: ["**/*.spec.ts", "**/*.int.ts"],
   roots: ["<rootDir>/src", "<rootDir>/tests"],
   transform: {
-    "\\.ts$": "ts-jest",
+    "\\.ts$": "ts-jest"
   },
-  clearMocks: true,
+  clearMocks: true
 };
